f692d04a1e4df81de1a9d5e759319aa795e6daa3
dzm <2545712559@qq.com>
2018-02-28 14:34:44 +0800

    javamarkdown v1

---
+++ src/com/dzm/gui/MarkDown.java
@@ -0,0 +1,34 @@
+package com.dzm.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*
+ * java markdownÖ÷½çÃæ
+ */
+public class MarkDown extends Application{
+	
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+            // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("main1.fxml"));
+
+            primaryStage.setTitle("JavaMarkDown");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+}
